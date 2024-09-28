377ff2c67258de2fc663a4e65b4be0678644da62
JongHyunJung <jjh19960115@gmail.com>
2024-09-29 00:41:40 +0900

    feat(BE): User 서비스 구현
    - UserService 파일 생성
    - UserRepository 파일 생성
    - User 공통 의존성 정의 파일 생성

---
+++ app/backend/src/main/domains/user/repositories.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+class UserRepository:
+    def get_user(self, db: Session, user_id: int):
+        return db.query(models.User).filter(models.User.id == user_id).first()
+
+    def create_user(self, db: Session, user: schemas.UserCreate):
+        db_user = models.User(email=user.email, hashed_password=user.password)  # 실제로는 비밀번호를 해시화해야 합니다
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
---
+++ app/backend/src/main/domains/user/services.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+
+from app.backend.src.main.domains.user.repositories import UserRepository
+
+class UserService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    def get_user(self, db: Session, user_id: int):
+        return self.user_repository.get_user(db, user_id)
