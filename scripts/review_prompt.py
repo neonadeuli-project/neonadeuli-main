afe21bd78bda48e5d77c26e7be64f5a9a5da0752
JongHyunJung <jjh19960115@gmail.com>
2024-10-08 08:11:02 +0900

    refactor(BE): AI 코드 리뷰 프롬프트 수정

--- scripts/review_prompt.py
+++ scripts/review_prompt.py
@@ -1,21 +1,66 @@
-review_prompt = """
+REVIEW_PROMPT = """
 당신은 전문적인 코드 리뷰어입니다. 주어진 코드를 분석하고 다음 형식으로 리뷰를 제공해주세요:
 
-안녕하세요! 리뷰봇입니다.
+## 전체 코드 리뷰
 
-1. **좋은 점**:
-   * [좋은 점 나열]
+### 좋은 점
+[좋은 점 나열]
 
-2. **개선할 점**:
-   * [개선이 필요한 점 나열]
+### 개선할 점
+[개선이 필요한 점 나열]
 
-3. **제안 사항**:
-   * [구체적인 개선 제안, 코드 예시 포함]
-
-수고하셨습니다! 코드 리뷰를 통해 개선할 수 있는 부분이 있으니, 이를 참고하여 더욱 나은 코드로 발전시켜 주시기 바랍니다.👍
+### 제안 사항
+[구체적인 개선 제안, 코드 예시 포함]
 
 전체 코드:
-{all_code}
+{code}
 
 응답은 위의 형식을 따라 작성해 주시되, 구체적이고 건설적인 피드백을 제공해 주세요.
 """
+
+FILE_REVIEW_PROMPT = """
+다음 {filename} 파일의 코드를 리뷰하고, 아래 형식으로 상세한 리뷰를 제공해주세요:
+
+## {filename} 파일 리뷰
+
+### 주요 기능
+[파일의 주요 기능 설명]
+
+### 좋은 점
+[좋은 점 나열]
+
+### 개선할 점
+[개선이 필요한 점 나열]
+
+### 제안 사항
+[구체적인 개선 제안, 코드 예시 포함]
+
+파일 내용:
+{content}
+"""
+
+LINE_COMMENTS_PROMPT = """
+다음 {filename} 파일의 코드를 리뷰하고, 중요한 라인에 대해 구체적인 코멘트를 제공해주세요. 
+형식은 '라인 번호: 코멘트'로 해주세요.
+
+{content}
+"""
+
+OVERALL_COMMENTS_PROMPT = """
+## AI 코드 리뷰 요약
+
+{overall_review}
+"""
+
+def get_review_prompt(code):
+    return REVIEW_PROMPT.format(code=code)
+
+def get_file_review_prompt(filename, content):
+    return FILE_REVIEW_PROMPT.format(filename=filename, content=content)
+
+def get_line_comments_prompt(filename, content):
+    return LINE_COMMENTS_PROMPT.format(filename=filename, content=content)
+
+def get_total_comments_prompt(overall_review):
+    return OVERALL_COMMENTS_PROMPT.format(overall_review=overall_review)
+    
