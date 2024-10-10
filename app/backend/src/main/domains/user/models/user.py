0e9a9f942b693a066319cc8bc1e17466410d8102
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 04:15:18 +0900

    feat(BE): User 엔티티 클래스 추가

--- app/backend/src/main/domains/user/models/user.py
+++ app/backend/src/main/domains/user/models/user.py
@@ -3,20 +3,28 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
+from src.main.core.enum import UserRole
 from src.main.db.database import Base
     
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    profile_image = Column(String(255), nullable=True)
+    is_active = Column(Boolean, default=True)
+    last_login = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
     # oauth_id = Column(String(255), unique=True, index=True, nullable=True)
-    # email = Column(String(255), unique=True, index=True)
-    name = Column(String(100))
-    # provider = Column(String(255), nullable=True)
-    # hashed_password = Column(String(255), nullable=True)
+    # oauth_provider = Column(String(50), nullable=True)
+
     # is_active = Column(Boolean, default=True)
     # role = Column(Enum(UserRole), default=UserRole.USER)
-    token = Column(String(255), unique=True, index=True)
+
+    # access_token = Column(String(255), unique=True, index=True, nullable=True)
+    # access_expires_at = Column(DateTime(timezone=True), nullable=True)
+
     # refresh_token = Column(String(255), unique=True, index=True, nullable=True)
-    # refresh_token_expires_at = Column(DateTime(timezone=True), nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    last_login = Column(DateTime(timezone=True), onupdate=func.now())
+    # refresh_expires_at = Column(DateTime(timezone=True), nullable=True)
+
