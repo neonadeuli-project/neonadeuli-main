a084fe6a0eae5a06b4977c1378766162e80f2107
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 04:11:41 +0900

    feat(BE): 소셜 로그인 Factory 구현

---
+++ app/backend/src/main/domains/user/auth/factory.py
@@ -0,0 +1,14 @@
+from src.main.core.auth.oauth import oauth
+from .providers.google import GoogleLogin
+
+class SocialLoginFactory:
+    @staticmethod
+    def get_social_login(provider: str):
+        if provider == 'google':
+            return GoogleLogin(oauth.google)
+        # elif provider == 'naver':
+        #     return NaverLogin
+        # elif provider == 'kakao':
+        #     return KakaoLogin
+        else:
+            raise ValueError(f"지원하지 않는 제공자: {provider}")
