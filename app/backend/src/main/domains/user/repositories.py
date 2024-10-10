fa4d290cbc419c0fd69cc7bca82f06e403447b98
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 04:14:51 +0900

    feat(BE): 유저 생성 및 조회 쿼리 구현
    - 이메일을 통한 유저 조회 쿼리 추가
    - 유저 생성 쿼리 추가

--- app/backend/src/main/domains/user/repositories.py
+++ app/backend/src/main/domains/user/repositories.py
@@ -1,13 +1,29 @@
+from sqlalchemy.future import select
 from sqlalchemy.orm import Session
-from . import models, schemas
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.main.domains.user.schemas.user import UserCreate
+from src.main.core.exceptions import NotFoundError, ValidationError
+from src.main.domains.user.models.user import User
 
 class UserRepository:
-    def get_user(self, db: Session, user_id: int):
-        return db.query(models.User).filter(models.User.id == user_id).first()
-
-    # def create_user(self, db: Session, user: schemas.UserCreate):
-    #     db_user = models.User(email=user.email, hashed_password=user.password)  # 실제로는 비밀번호를 해시화해야 합니다
-    #     db.add(db_user)
-    #     db.commit()
-    #     db.refresh(db_user)
-    #     return db_user
+    def __init__(self, db: AsyncSession):
+        self.db = db
+    
+    async def get_user_by_email(self, email: str) -> User:
+        result = await self.db.execute(select(User).where(User.email == email))
+        user = result.scalar_one_or_none()
+        if not user:
+            raise NotFoundError(f"이메일이 {email} 인 유저가 존재하지 않습니다.")
+        return user
+    
+    async def create_user(self, user_create: UserCreate) -> User:
+        try:
+            user = User(**user_create.model_dump(exclude_unset=True))
+            self.db.add(user)
+            await self.db.commit()
+            await self.db.refresh(user)
+            return user
+        except Exception as e:
+            await self.db.rollback()
+            raise ValidationError(f"Error creating user: {str(e)}")
