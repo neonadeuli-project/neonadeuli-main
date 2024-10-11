ce30398b58a25be75e0fdddc8e34d3c702a64068
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 15:49:23 +0900

    refactor(BE): 사용자 생성 스키마 값 추가
    - is_active 값 추가 (기본값 True)
    - password 값 None 옵션 추가

--- app/backend/src/main/domains/user/schemas/user/user_base.py
+++ app/backend/src/main/domains/user/schemas/user/user_base.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 class UserBase(BaseModel):
     email: EmailStr
-    name: str
+    name: str = Field(..., min_length=1, max_length=100)
 
     class Config:
         from_attributes = True
