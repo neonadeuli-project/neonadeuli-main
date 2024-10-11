406d1a5be9759e8a0f17d30a70b4473714d4d49b
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 16:38:39 +0900

    refactor(BE): 유저 생성 및 조회 쿼리 수정
    - 유효성 검사 코드 Service 단으로 이동

--- app/backend/src/main/domains/user/repositories.py
+++ app/backend/src/main/domains/user/repositories.py
@@ -1,29 +1,23 @@
+from asyncio.log import logger
 from sqlalchemy.future import select
-from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.main.core.auth.password import hash_password
 from src.main.domains.user.schemas.user import UserCreate
-from src.main.core.exceptions import NotFoundError, ValidationError
 from src.main.domains.user.models.user import User
 
 class UserRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
     
-    async def get_user_by_email(self, email: str) -> User:
+    async def get_user_by_email(self, email: str) -> User | None:
         result = await self.db.execute(select(User).where(User.email == email))
-        user = result.scalar_one_or_none()
-        if not user:
-            raise NotFoundError(f"이메일이 {email} 인 유저가 존재하지 않습니다.")
-        return user
+        return result.scalar_one_or_none()
     
-    async def create_user(self, user_create: UserCreate) -> User:
-        try:
-            user = User(**user_create.model_dump(exclude_unset=True))
-            self.db.add(user)
-            await self.db.commit()
-            await self.db.refresh(user)
-            return user
-        except Exception as e:
-            await self.db.rollback()
-            raise ValidationError(f"Error creating user: {str(e)}")
+    
+    async def create_user(self, user_data: dict) -> User:
+        user = User(**user_data)
+        self.db.add(user)
+        await self.db.commit()
+        await self.db.refresh(user)
+        return user
