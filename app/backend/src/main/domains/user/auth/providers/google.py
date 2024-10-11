4d98f6578b0bc2ce9c8e01dff7947612c1037716
JongHyunJung <jjh19960115@gmail.com>
2024-10-11 15:53:43 +0900

    refactor(BE): 구글 OAuth 로직 수정
    - state 값 매개변수 추가
    - @classmethod 어노테이션 제거
    - 토큰에서 user_info 추출 로직 수정
    - GoogleUserInfo 응답 값으로 반환

--- app/backend/src/main/domains/user/auth/providers/google.py
+++ app/backend/src/main/domains/user/auth/providers/google.py
@@ -1,4 +1,5 @@
 from fastapi import Request
+from src.main.domains.user.schemas.social_auth import GoogleUserInfo
 from src.main.domains.user.schemas.user import UserCreate
 from src.main.domains.user.auth.base import SocialLoginBase
 
@@ -7,17 +8,20 @@ class GoogleLogin(SocialLoginBase):
     def __init__(self, oauth_client):
         self.oauth_client = oauth_client
 
-    @classmethod
-    async def get_authorization_url(cls, request: Request) -> str:
+    async def get_authorization_url(self, request: Request, state: str) -> str:
         redirect_uri = request.url_for('auth_callback', provider='google')
-        return await cls.oauth_client.authorize_redirect(request, redirect_uri)
+        print(f"Generated redirect_uri: {redirect_uri}")  # 리디렉션 URI 출력
+        return await self.oauth_client.authorize_redirect(request, redirect_uri, state=state)
     
-    @classmethod
-    async def get_user_info(cls, request: Request) -> UserCreate:
-        token = await cls.oauth_client.authorize_access_token(request)
-        user_info = await cls.oauth_client.authorize_access_token(request)
-        return UserCreate(
+    async def get_user_info(self, request: Request) -> UserCreate:
+        token = await self.oauth_client.authorize_access_token(request)
+        print(f"Token received: {token}")
+
+        user_info = token.get("userinfo")
+        google_user = GoogleUserInfo(
             email=user_info['email'],
             name=user_info['name'],
             profile_image=user_info.get('picture')
         )
+
+        return google_user.to_user_create()
