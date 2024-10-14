ae68299be07a9969cb3e149b2d5666b9895cd16c
JongHyunJung <jjh19960115@gmail.com>
2024-10-14 20:08:29 +0900

    refactor(BE): setup_oauth 메서드 예외처리 추가

--- app/backend/src/main/core/auth/oauth.py
+++ app/backend/src/main/core/auth/oauth.py
@@ -1,5 +1,6 @@
 from asyncio.log import logger
 from authlib.integrations.starlette_client import OAuth
+from src.main.core.exceptions import InternalServerError
 from src.main.core.config import settings
 
 oauth = OAuth()
@@ -7,14 +8,16 @@ oauth = OAuth()
 def setup_oauth():
     if not settings.GOOGLE_CLIENT_ID or not settings.GOOGLE_CLIENT_SECRET:
         raise ValueError("Google OAuth 자격 증명이 제대로 구성되지 않았습니다.")
-
-    oauth.register(
-        name='google',
-        client_id=settings.GOOGLE_CLIENT_ID,
-        client_secret=settings.GOOGLE_CLIENT_SECRET,
-        server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-        client_kwargs={'scope': 'openid email profile'}
-    )
+    try:
+        oauth.register(
+            name='google',
+            client_id=settings.GOOGLE_CLIENT_ID,
+            client_secret=settings.GOOGLE_CLIENT_SECRET,
+            server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+            client_kwargs={'scope': 'openid email profile'}
+        )
+    except Exception as e:
+        raise InternalServerError(f"OAuth 설정 중 오류 발생: {str(e)}")
 
     logger.info("Google OAuth 클라이언트가 성공적으로 등록되었습니다.")
 
