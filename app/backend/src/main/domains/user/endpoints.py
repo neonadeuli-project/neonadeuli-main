32f684f7e7cc358e6864ac815de8f01fe8e40f01
JongHyunJung <jjh19960115@gmail.com>
2024-10-14 20:02:24 +0900

    refactor(BE): OAuth 로그인 엔드포인트 수정
    - 예외 처리 코드 추가
    - 소셜 로그인 반환 값 수정

--- app/backend/src/main/domains/user/endpoints.py
+++ app/backend/src/main/domains/user/endpoints.py
@@ -1,14 +1,15 @@
-from asyncio.log import logger
 import secrets
-from fastapi import APIRouter, Depends, HTTPException, Query, Request
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, Query, Request
+from fastapi.responses import JSONResponse, RedirectResponse
 
-from src.main.db.deps import get_db
+from src.main.domains.user.schemas.user.user_response import UserResponse
+from src.main.domains.user.schemas.auth import AuthResponse
 from src.main.domains.user.models.user import User
 from src.main.core.auth.jwt import create_access_token
 from src.main.domains.user.services import UserService
 from src.main.core.exceptions import (
-    AuthenticationError, 
+    AuthenticationError,
+    InternalServerError, 
     NotFoundError, 
     ValidationError
 )
@@ -20,44 +21,47 @@ from .dependencies import get_user_service
 router = APIRouter()
 
 @router.get("/login/{provider}")
-async def social_login(
-    request: Request,
-    provider: str
-):
+async def social_login(request: Request,provider: str):
     try:
         social_login_class = SocialLoginFactory.get_social_login(provider)
         state = secrets.token_urlsafe()
         request.session['oauth_state'] = state
         print(f"Generated state: {state}") 
-        return await social_login_class.get_authorization_url(request, state)
+        auth_url = await social_login_class.get_authorization_url(request, state)
+        return JSONResponse(content={"auth_url": auth_url})
     except ValueError:
         raise NotFoundError(f"지원하지 않는 제공자 : {provider}")
+    except Exception as e:
+        raise InternalServerError(f"로그인 처리 중 오류 발생: {str(e)}")
 
 @router.get("/auth/{provider}/callback")
 async def auth_callback(
     request: Request,
     provider: str,
     state: str = Query(...),
-    user_service: UserService = Depends(get_user_service),
-    db: AsyncSession = Depends(get_db)
+    user_service: UserService = Depends(get_user_service)
 ):
     try:
         stored_state = request.session.get('oauth_state')
         if state != stored_state:
-            raise HTTPException(status_code=400, detail="Invalid state parameter")
+            raise ValidationError("유효하지 않은 State 파라미터입니다.")
             
         social_login_class = SocialLoginFactory.get_social_login(provider)
         user_create = await social_login_class.get_user_info(request)
 
-        user = await user_service.get_or_create_user(db, user_create)
-
+        user = await user_service.get_or_create_user(user_create)
         access_token = create_access_token(data={"sub": user.email})
-        return {"access_token": access_token, "token_type": "bearer"}
+
+        auth_response = AuthResponse.create(UserResponse.from_orm(user), access_token)
+
+        return auth_response.to_response()
     
     except ValueError:
         raise NotFoundError(f"지원하지 않는 제공자: {provider}")
-    except (AuthenticationError, ValidationError) as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    except AuthenticationError as e:
+        raise AuthenticationError(str(e))
+    except Exception as e:
+        raise InternalServerError(f"인증 콜백 처리 중 오류 발생: {str(e)}")
 
 @router.get("/token/refresh")
 async def refresh_token(refresh_token: str):
