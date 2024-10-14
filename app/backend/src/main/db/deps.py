bcdb399080305d4e81089d481164529f46bb2943
JongHyunJung <jjh19960115@gmail.com>
2024-10-14 20:07:28 +0900

    refactor(BE): DB 세션 관련 예외 처리

--- app/backend/src/main/db/deps.py
+++ app/backend/src/main/db/deps.py
@@ -2,7 +2,13 @@ from src.main.db.database import AsyncSessionLocal
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        yield session
-        await session.commit()
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
         # with을 사용하면 알아서 session을 닫아줌
         # await session.close()
