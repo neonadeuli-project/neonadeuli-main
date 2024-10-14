9d7733f85119e7106c3d485394f5d8b2b49b4040
JongHyunJung <jjh19960115@gmail.com>
2024-10-14 20:09:28 +0900

    refactor(BE): 유저 응답 DTO 수정
    - last_login, created_at 타입 변경 (datetime -> str)
    - from_orm 메서드 생성

--- app/backend/src/main/domains/user/schemas/user/user_response.py
+++ app/backend/src/main/domains/user/schemas/user/user_response.py
@@ -6,8 +6,20 @@ class UserResponse(UserBase):
     id: int
     profile_image: HttpUrl | None = None
     is_active: bool
-    last_login: datetime | None
-    created_at: datetime
+    last_login: str | None
+    created_at: str
 
     class Config:
         from_attributes = True
+
+    @classmethod
+    def from_orm(cls, db_user):
+        return cls(
+            id=db_user.id,
+            email=db_user.email,
+            name=db_user.name,
+            profile_image=db_user.profile_image,
+            is_active=db_user.is_active,
+            last_login=db_user.last_login.isoformat() if isinstance(db_user.last_login, datetime) else db_user.last_login,
+            created_at=db_user.created_at.isoformat() if isinstance(db_user.created_at, datetime) else db_user.created_at
+        )
