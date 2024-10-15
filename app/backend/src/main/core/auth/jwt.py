7a69de6acdd330e8fbf56428f07afbf52d994dad
JongHyunJung <jjh19960115@gmail.com>
2024-10-15 19:51:38 +0900

    feat(BE): refresh token 생성 메소드 추가

--- app/backend/src/main/core/auth/jwt.py
+++ app/backend/src/main/core/auth/jwt.py
@@ -1,13 +1,20 @@
 from datetime import datetime, timedelta
-
-from fastapi import HTTPException, status
-from src.main.core.config import settings
 from jose import JWTError, jwt
+
+from src.main.core.exceptions import AuthenticationError
+from src.main.core.config import settings
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     expire = datetime.now() + (expires_delta or timedelta(minutes=15))
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire, "type": "access"})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now() + (expires_delta or timedelta(days=7))
+    to_encode.update({"exp": expire, "type": "refresh"})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
@@ -16,6 +23,5 @@ def verify_token(token: str):
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         return payload
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="자격 증명을 검증할 수 없습니다.")
-
-# TODO: 추후 Refresh Token 관련 함수 구현
+        raise AuthenticationError("자격 증명을 검증할 수 없습니다.")
+    
