435ba8a7ea6445aeb44b2c80043670ed5608f8ce
JongHyunJung <jjh19960115@gmail.com>
2024-10-15 19:49:10 +0900

    refactor(BE): redis 인증 의존성 메서드 추가
    - get_auth_service 메서드 추가
    - get_redis_token_manager, get_redis_client 메서드 추가

--- app/backend/src/main/domains/user/dependencies.py
+++ app/backend/src/main/domains/user/dependencies.py
@@ -1,11 +1,28 @@
 from fastapi import Depends
+from redis import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.main.domains.user.service.auth_service import AuthService
+from src.main.core.auth.token_manager import RedisTokenManager
 from .service.user_service import UserService
 from .repositories import UserRepository
 from src.main.db.deps import get_db
+from src.main.db.database import redis_client
+
+def get_redis_client() -> Redis:
+    return redis_client
 
 def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserRepository:
     return UserRepository(db)
 
 def get_user_service(db: AsyncSession = Depends(get_db)) -> UserService:
     return UserService(db)
+
+def get_redis_token_manager(redis_client: Redis = Depends(get_redis_client)) -> RedisTokenManager:
+    return RedisTokenManager(redis_client)
+
+def get_auth_service(
+        user_service: UserService = Depends(get_user_service),
+        token_manager: RedisTokenManager = Depends(get_redis_token_manager)
+) -> AuthService:
+    return AuthService(user_service, token_manager)
