1d0a3e6efbdd233cb9bef3e9b7c049f1e78020e4
JongHyunJung <jjh19960115@gmail.com>
2024-10-19 09:16:39 +0900

    refactor(BE): 로그인 주요 서비스 로직 수정

--- app/backend/src/main/domains/user/service/auth_service.py
+++ app/backend/src/main/domains/user/service/auth_service.py
@@ -1,23 +1,27 @@
+import json
 import logging
 import secrets
 
 from datetime import timedelta
 from asyncio.log import logger
-from fastapi import HTTPException, Request
-from fastapi.responses import JSONResponse
-from jose import JWTError
+from fastapi import Request
+from fastapi.responses import JSONResponse, RedirectResponse
+import urllib
 
+from src.main.domains.user.schemas.user.user_create import UserCreate
 from src.main.domains.user.service.user_service import UserService
 from src.main.domains.user.repository.user_repository import UserRepository
 from src.main.domains.user.schemas.user.user_response import UserResponse
 from src.main.domains.user.auth.factory import SocialLoginFactory
 from src.main.domains.user.repository.token_repository import TokenRepository
+
+from src.main.core.config import settings
 from src.main.core.exceptions import (
     AuthenticationError,
     InternalServerError, 
-    NotFoundError
+    NotFoundError,
+    ValidationError
 )
-from src.main.core.config import settings
 from src.main.core.auth.jwt import (
     create_access_token, 
     create_refresh_token, 
@@ -27,41 +31,40 @@ from src.main.core.auth.jwt import (
 logger = logging.getLogger(__name__)
 
 class AuthService:
-    def __init__(self, user_repository: UserRepository, token_repository: TokenRepository):
-        self.user_repository = user_repository
+    def __init__(self, user_service: UserService, token_repository: TokenRepository):
+        self.user_service = user_service
         self.token_repository = token_repository
 
-    async def initialize_social_login(self, request: Request, provider: str) -> str:
+    async def initialize_social_login(self, provider: str) -> str:
         try:
-            state = secrets.token_urlsafe(32)
-            social_login_class = SocialLoginFactory.get_social_login(provider)
-            auth_url = await social_login_class.get_authorization_url(request, state)
+            logger.info(f"소셜 로그인 초기화 중... provider: {provider}")
+            social_login = SocialLoginFactory.get_social_login(provider, self.token_repository)
+            auth_url = await social_login.get_authorization_url()
+            logger.info(f"Authorization URL 생성 완료! auth_url: {auth_url}")
+            return auth_url
 
-            self.token_repository.store_oauth_state(state, provider, 300)
-
-            return {
-                "auth_url": auth_url,
-                "state": state
-            }
         except Exception as e:
             logger.exception(f"소셜 로그인 초기화 중 오류 발생: {str(e)}")
             raise InternalServerError(f"소셜 로그인 초기화 중 오류 발생: {str(e)}")
     
     async def handle_oauth_callback(self, request: Request, provider: str) -> JSONResponse:
+        code = request.query_params.get('code')
         state = request.query_params.get('state')
-        if not state:
-            raise HTTPException(status_code=400, detail="State parameter is missing")
+        logger.debug(f"전달 받은 state: {state}, provider: {provider}, code: {code}")
+        
+        if not code or not state:
+            raise AuthenticationError("코드 또는 상태 매개 변수가 누락되었습니다.")
 
         # Redis에서 상태 검증
-        stored_provider = self.token_repository.get_oauth_state(state)
-        if not stored_provider or stored_provider != provider:
-            raise HTTPException(status_code=400, detail="Invalid state parameter")
+        if not await self.token_repository.verify_oauth_state(state, provider):
+            raise ValidationError("유효하지 않은 상태 매개변수입니다.")
+        
         try:
-            social_login_class = SocialLoginFactory.get_social_login(provider)
-            user_create = await social_login_class.get_user_info(request)
+            social_login = SocialLoginFactory.get_social_login(provider, self.token_repository)
+            user_create = await social_login.get_user_info(code, state)
             logger.debug(f"생성된 UserCreate 객체: {user_create}")
             
-            user = await self.user_repository.get_or_create_user(user_create)
+            user = await self.user_service.get_or_create_user(user_create)
 
             access_token = create_access_token(data={"sub": user.email})
             refresh_token = create_refresh_token(data={"sub": str(user.id)})
@@ -78,17 +81,22 @@ class AuthService:
             # 사용된 OAuth 상태 삭제
             self.token_repository.delete_oauth_state(state)
 
-            # Set cookies for access and refresh tokens
-            response = JSONResponse(content={"message": "로그인 성공", "user": UserResponse.from_orm(user).dict()})
-            response.headers["Location"] = "http://localhost:3000/dashboard"
-            response.status_code = 302 
+            user_data = UserResponse.from_orm(user).model_dump()
+            encoded_user_data = urllib.parse.quote(json.dumps(user_data))
+            
+            redirect_url = f"http://localhost:3000/dashboard?user={encoded_user_data}"
+            
+            response = RedirectResponse(url=redirect_url)
+
             response.set_cookie(
                 key="access_token", 
                 value=access_token, 
                 httponly=True,
                 secure=False,
                 samesite="lax",
-                max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 24 * 60 * 60
+                max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 24 * 60 * 60,
+                domain="localhost",
+                path="/"
             )
             response.set_cookie(
                 key="refresh_token", 
@@ -96,9 +104,12 @@ class AuthService:
                 httponly=True,
                 secure=False,
                 samesite="lax",
-                max_age=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60
+                max_age=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60,
+                domain="localhost",
+                path="/"
             )
 
+            logger.info(f"사용자 정보와 토큰을 포함한 응답을 전송합니다: {response.body}")
             return response
 
         except AuthenticationError as e:
@@ -163,6 +174,12 @@ class AuthService:
             logger.exception(f"Unexpected error in refresh_token: {str(e)}")
             raise InternalServerError(f"예상치 못한 에러 발생: {str(e)}")
 
-    async def logout(self, user_id: int, token: str):
-        await self.token_repository.blacklist_token(token)
-        await self.token_repository.delete_refresh_token(str(user_id))
+    async def logout(self, token: str, user_email: str):
+        try:
+            logger.info(f"Attempting to logout user: {user_email}")
+            await self.token_repository.blacklist_token(token)
+            await self.token_repository.delete_refresh_token(user_email)
+            logger.info(f"이메일이 {user_email} 인 유저가 성공적으로 로그아웃했습니다.")
+        except Exception as e:
+            logger.error(f"로그아웃 과정 도중 에러 발생: {str(e)}", exc_info=True)
+            raise InternalServerError(f"로그아웃 처리 중 예상치 못한 에러 발생: {str(e)}")
